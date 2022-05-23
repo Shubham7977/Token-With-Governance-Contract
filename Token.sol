a504c1e6bb5999c79673d943e7f6d10abbfddf3a
Shubham Baroliya <shubham.baroliya@minddeft.net>
2022-05-23 12:11:38 +0530

    edited

--- Token.sol
+++ Token.sol
@@ -13,8 +13,7 @@ contract MyToken is ERC20, Ownable {
         mint(0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2, 10);
         mint(0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db, 10);
         mint(0x78731D3Ca6b7E34aC0F824c42a7cC18A495cabaB, 10);
-        mint(0x617F2E2fD72FD9D5503197092aC168c91465E7f2, 10); 
-        //["0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2","0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db"];
+        mint(0x617F2E2fD72FD9D5503197092aC168c91465E7f2, 10);
     }
 
     function mint(address to, uint256 amount) public onlyOwner {
